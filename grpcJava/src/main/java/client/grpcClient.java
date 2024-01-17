c4dca1842853a2a4d325a3d9f78f91d75a2751f4
Harshith VH <vhharshith77@gmail.com>
2024-01-17 17:51:56 +0530

    Refactored client responses

--- grpcJava/src/main/java/client/grpcClient.java
+++ grpcJava/src/main/java/client/grpcClient.java
@@ -1,8 +1,9 @@
 package client;
 
+import com.grpcTrainbooking.proto.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import com.grpcTrainbooking.proto.*;
+import io.grpc.StatusRuntimeException;
 
 public class grpcClient {
 
@@ -12,24 +13,144 @@ public class grpcClient {
                 .usePlaintext()
                 .build();
 
-        // Create a stub for the PurchaseTicket service
-        PurchaseTicketServiceGrpc.PurchaseTicketServiceBlockingStub stub = PurchaseTicketServiceGrpc.newBlockingStub(channel);
+        PurchaseTicketServiceGrpc.PurchaseTicketServiceBlockingStub ticketStub = PurchaseTicketServiceGrpc.newBlockingStub(channel);
+        
+        AllocateSeatServiceGrpc.AllocateSeatServiceBlockingStub allocateSeatStub = AllocateSeatServiceGrpc.newBlockingStub(channel);
+        
+        ShowReceiptServiceGrpc.ShowReceiptServiceBlockingStub receiptStub = ShowReceiptServiceGrpc.newBlockingStub(channel);
+        
+        GetUsersBySectionServiceGrpc.GetUsersBySectionServiceBlockingStub sectionStub = GetUsersBySectionServiceGrpc.newBlockingStub(channel);
+        
+        RemoveUserServiceGrpc.RemoveUserServiceBlockingStub removeUserStub = RemoveUserServiceGrpc.newBlockingStub(channel);
+        
+        ModifySeatServiceGrpc.ModifySeatServiceBlockingStub modifySeatStub = ModifySeatServiceGrpc.newBlockingStub(channel);
 
-        // Prepare a PurchaseRequest
+        purchaseTicket(ticketStub);
+        
+        allocateSeat(allocateSeatStub);
+        
+        showReceipt(receiptStub);
+        
+        getUsersBySection(sectionStub);
+        
+        removeUser(removeUserStub);
+        
+        modifySeat(modifySeatStub);
+
+        channel.shutdown();
+    }
+
+    private static void purchaseTicket(PurchaseTicketServiceGrpc.PurchaseTicketServiceBlockingStub stub) {
+    	
         PurchaseRequest purchaseRequest = PurchaseRequest.newBuilder()
                 .setFrom("London")
                 .setTo("France")
                 .setUser(User.newBuilder().setFirstName("John").setLastName("Doe").setEmail("john.doe@example.com").build())
                 .build();
-
-        // Call the PurchaseTicket API
-        PurchaseResponse purchaseResponse = stub.purchaseTicket(purchaseRequest);
-
-        // Print the response
-        System.out.println("Purchase ID: " + purchaseResponse.getPurchaseId());
-        System.out.println("Price Paid: " + purchaseResponse.getPricePaid());
-
-        // Shutdown the channel
-        channel.shutdown();
+        
+        try {
+        	
+        	PurchaseResponse purchaseResponse = stub.purchaseTicket(purchaseRequest);
+        	
+        	System.out.println("Purchase ID: " + purchaseResponse.getPurchaseId());
+            System.out.println("Price Paid: " + purchaseResponse.getPricePaid());
+            
+        } catch (StatusRuntimeException e) {
+            System.err.println("Error calling PurchaseTicket: " + e.getStatus());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        
+        
     }
+    
+    private static void allocateSeat(AllocateSeatServiceGrpc.AllocateSeatServiceBlockingStub stub) {
+
+    	AllocateSeatRequest allocateSeatRequest = AllocateSeatRequest.newBuilder()
+                .setEmail("john.doe@example.com")
+                .setSection("A")
+                .build();
+        
+        try {
+        	
+        	AllocateSeatResponse allocateSeatResponse = stub.allocateSeat(allocateSeatRequest);
+        	
+            System.out.println("Seat Allocated: " + allocateSeatResponse.getSection() + " - " + allocateSeatResponse.getSeatNumber());
+
+        } catch (StatusRuntimeException e) {
+            System.err.println("Error calling AllocateSeat: " + e.getStatus());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        
+    }
+    
+    private static void showReceipt(ShowReceiptServiceGrpc.ShowReceiptServiceBlockingStub stub) {
+        ShowReceiptRequest showReceiptRequest = ShowReceiptRequest.newBuilder()
+                .setEmail("john.doe@example.com")
+                .build();
+
+        try {
+            ShowReceiptResponse receiptResponse = stub.showReceipt(showReceiptRequest);
+            System.out.println("Receipt : " + receiptResponse.getUserInfo().getPurchaseId());
+        } catch (StatusRuntimeException e) {
+            System.err.println("Error calling ShowReceipt: " + e.getStatus());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    
+    private static void getUsersBySection(GetUsersBySectionServiceGrpc.GetUsersBySectionServiceBlockingStub stub) {
+        GetUsersBySectionRequest getUsersBySectionRequest = GetUsersBySectionRequest.newBuilder()
+                .setSection("A")
+                .build();
+
+        try {
+            GetUsersBySectionResponse usersBySectionResponse = stub.getUsersBySection(getUsersBySectionRequest);
+
+            System.out.println("UsersBySection: ");
+            for (Receipt userReceipt : usersBySectionResponse.getUserInfoList()) {
+            	System.out.println("User Email: " + userReceipt.getUser().getEmail());
+                System.out.println("Purchase ID: " + userReceipt.getPurchaseId());
+            }
+
+        } catch (StatusRuntimeException e) {
+            System.err.println("Error calling GetUsersBySection: " + e.getStatus());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    
+    private static void removeUser(RemoveUserServiceGrpc.RemoveUserServiceBlockingStub stub) {
+        RemoveUserRequest removeUserRequest = RemoveUserRequest.newBuilder()
+                .setEmail("john.doe@example.com")
+                .build();
+
+        try {
+            RemoveUserResponse removeUserResponse = stub.removeUser(removeUserRequest);
+            System.out.println("RemoveUser Response: " + removeUserResponse.getRes());
+        } catch (StatusRuntimeException e) {
+            System.err.println("Error calling RemoveUser: " + e.getStatus());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    
+    private static void modifySeat(ModifySeatServiceGrpc.ModifySeatServiceBlockingStub stub) {
+        ModifySeatRequest modifySeatRequest = ModifySeatRequest.newBuilder()
+                .setEmail("john.doe@example.com")
+                .setNewSection("B")
+                .setNewSeatNumber(5)
+                .build();
+
+        try {
+            ModifySeatResponse modifySeatResponse = stub.modifySeat(modifySeatRequest);
+            System.out.println("ModifySeat Response: " + modifySeatResponse.getRes());
+        } catch (StatusRuntimeException e) {
+            System.err.println("Error calling ModifySeat: " + e.getStatus());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
 }
